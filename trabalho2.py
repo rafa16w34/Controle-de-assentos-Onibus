be36e7176567ec66302b9d5d37a1b27eab5018b0
Rafael Alves <rafael16alvesf@gmail.com>
2025-11-18 16:24:52 -0300

    Atualização
    Agora o usuário digita o horário do ônibus e alguns bugs foram corrigidos.

--- trabalho2.py
+++ trabalho2.py
@@ -3,45 +3,61 @@ import datetime as dt
 
 data_atual = dt.datetime.now()
 
-hora_atual = data_atual.hour
-minuto_atual = data_atual.minute
-
 linhas = {'Cidade de origem':[],'Cidade de destino':[],'Horário de partida':[],'Valor da passagem':[], 'Ônibus': [] }#Dicionário da linha
 
 onibus = {'Data da partida': [], 'Assentos Disponíveis':[]}#Dicionário do ônibus
 
+#############################################################################################################################################################
 
+#Funções:
 
-
-
+#Criar uma linha com as informações fornecidadas:
 
 def cadastroLinhas():# Preenche a linha com os dados que o usuario fornece
 
-    linhas['Cidade de origem'].append(str(input('\nDigite o nome da cidade de origem da linha:\n-> ')))
-    linhas['Cidade de destino'].append(str(input('\nDigite o nome da cidade de destino da linha:\n-> ')))
-    linhas['Horário de partida'].append((f'{hora_atual}:{minuto_atual}'))
-    
-    try:
+    cidade_origem_digitada = str(input('\nDigite o nome da cidade de origem da linha:\n-> '))
 
-        linhas['Valor da passagem'].append(int(input('\nDigite o valor em reais da passagem:\n-> R$')))
+    cidade_destino_digitada = str(input('\nDigite o nome da cidade de destino da linha:\n-> '))
 
-    except(ValueError):
+    hora_digitada = str(input('\nDigite a hora que ele irá sair: \nFormato: ( 00:00 )\n-> '))
 
-        print('\nErro: Digite um número inteiro!\n')
+    if (hora_digitada.find(':') == 2):
+
+        hora_digitada = hora_digitada.split(':')
+
+        hora_atual = int(hora_digitada[0])
+        minuto_atual = int(hora_digitada[1])
+
+        if (hora_atual < 24) and (hora_atual >= 0) and (minuto_atual < 59) and (minuto_atual >= 0):
+
+            linhas['Cidade de origem'].append(cidade_origem_digitada)
+            linhas['Cidade de destino'].append(cidade_destino_digitada)
+
+            linhas['Horário de partida'].append((f'{hora_atual}:{minuto_atual}'))
+            
+            try:
+
+                linhas['Valor da passagem'].append(int(input('\nDigite o valor em reais da passagem:\n-> R$')))
+
+            except(ValueError):
+
+                print('\nErro: Digite um número inteiro!\n')
+                pass
+
+            linhas['Ônibus'].append((gerar_onibus()))
+
+        else:
+
+            pass
+
+    else:
+
+        print('\nErro: Digite uma data no formato correto!\n')
         pass
-
-    linhas['Ônibus'].append((gerar_onibus()))
     
-    
+#####################################################################################################################################################
 
-
-
-
-
-
-
-
-
+#Gera um ônibus vazio:
 
 def gerar_onibus():#Função que cria o ônibus
 
@@ -49,15 +65,57 @@ def gerar_onibus():#Função que cria o ônibus
     
     return assentos
 
+###################################################################################################################################################
+
+#Criar outro ônibus para uma linha já existente:
+
+
+def DuplicarLinhas(linha_escolhida):#Duplica as informações da linha da qual se quer duplicar o onibus, mantendo as informações mas criando mais um ônibus vazio
+
+    linhas['Cidade de origem'].append(linhas['Cidade de origem'][linha_escolhida])
+    linhas['Cidade de destino'].append(linhas['Cidade de destino'][linha_escolhida])
+    linhas['Horário de partida'].append(linhas['Horário de partida'][linha_escolhida])
+    
+    try:
+
+        linhas['Valor da passagem'].append(linhas['Valor da passagem'][linha_escolhida])
+
+    except(ValueError):
+
+        print('\nErro: Digite um número inteiro!\n')
+        pass
+
+    linhas['Ônibus'].append((gerar_onibus()))
 
 
 
+def criar_outro_onibus():#Função que cria mais um ônibus para linha
+
+    
+    print('\nEscolha entre as linhas existentes aquela que você gostaria de adicionar um ônibus:\n')
+
+    try:
+
+        for i in range(len(linhas['Cidade de origem'])):
+
+            print(f'Linha {i} | {linhas["Cidade de origem"][i]} - {linhas["Cidade de destino"][i]}|\n')
+
+        linha_escolhida = int(input('\nDigite o número da linha :\n-> '))
+
+    except(ValueError):
+        print('\nErro: Digite um número inteiro!\n')
+        pass
+
+    DuplicarLinhas(linha_escolhida)
+
+    return 
+
+###########################################################################################################################################################
+
+#Marcar um assento:
 
 
-
-
-
-def escolher_onibus():
+def escolher_onibus():#Escolher um do ônibus para fazer a viagem
 
     print('\nÔnibus existentes:\n')
 
@@ -65,7 +123,7 @@ def escolher_onibus():
 
         for i in range(len(linhas['Ônibus'])):
 
-            print(f'Ônibus {i} | {linhas["Cidade de origem"]} - {linhas["Cidade de destino"]}|\n')
+            print(f'Ônibus {i} | {linhas["Cidade de origem"][i]} - {linhas["Cidade de destino"][i]}|\n')
 
         onibus_escolhido = int(input('\nDigite o número do ônibus:\n-> '))
 
@@ -77,45 +135,39 @@ def escolher_onibus():
 
 
 
-def preencher_onibus():
+def preencher_onibus():#Escolher qual será seu assento
 
-        onibus_escolhido = escolher_onibus()
+    onibus_escolhido = escolher_onibus()
+
+    onibus = linhas['Ônibus'][onibus_escolhido]
     
-        onibus = linhas['Ônibus'][onibus_escolhido]
-        
-        print(onibus)
+    print(onibus)
 
-        i = int(input(f"Digite qual assento você gostaria de ocupar (x): "))
-        j = int(input(f"Digite qual assento você gostaria de ocupar (y): "))
+    i = int(input(f"Digite qual assento você gostaria de ocupar (x): "))
+    j = int(input(f"Digite qual assento você gostaria de ocupar (y): "))
 
-        if(i< 5) and (j<4):
+    if(i< 5) and (j<4):
 
-            if onibus[i][j] != 1:
+        if onibus[i][j] != 1:
 
-                onibus[i][j] = 1
+            onibus[i][j] = 1
 
-            else:
-                print('\nErro: Esse assento já está ocupado.\n')
         else:
+            print('\nErro: Esse assento já está ocupado.\n')
 
-            print('\nErro: Esse assento não existe!\n')
 
+    else:
+
+        print('\nErro: Esse assento não existe!\n')
+
+    print(f'\nO valor será de : R$ {linhas["Valor da passagem"][onibus_escolhido]:.2f}\n')
+
+    print('\nCompra realizada com sucesso!\n')
+
+#####################################################################################################################################
+
+#Consulta os assentos do Ônibus
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def consultarAssentos():
 
     onibus_escolhido = escolher_onibus()
@@ -132,18 +184,28 @@ def consultarAssentos():
 
     return(onibus['Assentos Disponíveis'])
 
+############################################################################################################################################
 
-
+#Consulta os horários disponíveis em uma cidade
 
 def consultarHorarios():
 
     horarios_cidade = []
+    cidade_exibida = []
 
     print('\nCidades existentes:\n')
 
     for i in range(len(linhas['Cidade de origem'])):
 
-        if linhas['Cidade de origem'][i] != linhas['Cidade de origem'][i-1]:
+        if (len(linhas['Cidade de origem'])) > 1:
+
+            if ((linhas['Cidade de origem'][i]) not in cidade_exibida):
+
+                print(f'Cidade {i} | {linhas["Cidade de origem"][i]} |\n')  #Ele pode digitar um número i de uma das cidades repetidas
+                
+                cidade_exibida.append(linhas['Cidade de origem'][i])        #Posso usar essa lista para contornar esse erro
+
+        else:
 
             print(f'Cidade {i} | {linhas["Cidade de origem"][i]} |\n')
 
@@ -154,6 +216,7 @@ def consultarHorarios():
             
     except(ValueError):
         print('\nErro: Digite um número inteiro!\n')
+        pass
 
 
     for i in range(len(linhas['Cidade de origem'])):
@@ -165,7 +228,7 @@ def consultarHorarios():
 
     print(f'\nHorários de partida do ônibus: {horarios_cidade}')
 
-    
+#####################################################################################################################################################
 
 
 
@@ -182,19 +245,22 @@ o Uma reserva por linha.
     '''
     pass
 
-sair = 0 #Varíavel que controla o loop do while do menu
+#########################################################################################################################################################
 
-onibus_criado = False #Variável usada por mim somente para testar as funções, uma outra implementação deve ser desenvolvida para o menu
+# Menu principal
+
+sair = 0 #Varíavel que controla o loop do while do menu
 
 while sair == 0 :
 
     try:
         print("\nSistema da Rodoviária:")
-        print("1 - Cadastrar ou editar linhas ")
-        print("2 - Consultar horários disponíveis para uma cidade")
-        print("3 - Consultar os assentos disponíveis no ônibus")
-        print('4 - Marcar um assento de um ônibus')
-        print("0 - Sair")
+        print("1 - Cadastrar ou editar linhas;")
+        print("2 - Consultar horários disponíveis para uma cidade;")
+        print("3 - Consultar os assentos disponíveis no ônibus;")
+        print('4 - Marcar um assento de um ônibus;')
+        print('5 - Criar outro ônibus para uma linha já existente;')
+        print("0 - Sair.")
 
         opcao = int(input("Opção: "))
 
@@ -255,11 +321,21 @@ while sair == 0 :
 
                     preencher_onibus()
 
-                    confirmacao = str(input('\nDigite qualquer tecla para retornar ao menu.\n'))
+                    confirmacao = str(input('\nDigite a tecla "Enter" para retornar ao menu.\n'))
 
                 else:
                     print('\nErro: Nenhuma linha foi criada para que se possa verificar!\n')
 
+            case 5:#Cria outro ônibus para uma linha já existente
+
+                if (linhas['Ônibus']):
+
+                    criar_outro_onibus()
+
+                else:
+                    print('\nErro: Nenhuma linha foi criada para que se possa verificar!\n')
+                
+            
             case 0:
 
                 sair = 1
